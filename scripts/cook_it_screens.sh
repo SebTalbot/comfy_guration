9c6ec73866850be792acbb832e31ee43965b9db0
Sebastien Talbot <moi@sebastientalbot.com>
2018-05-24 16:50:51 -0400

    [Polybar] Fix index and colors

--- scripts/cook_it_screens.sh
+++ scripts/cook_it_screens.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 xrandr --output HDMI-2 --primary
 xrandr --output HDMI-1 --right-of HDMI-2
-feh --bg-scale ~/pictures/background/synthwave2.jpg
+feh --bg-fill ~/pictures/background/synthwave2.jpg
