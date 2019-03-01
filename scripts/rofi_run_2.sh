208a80d368be6132213a65c287ace50041bde861
Sebastien Talbot <moi@sebastientalbot.com>
2019-02-28 19:18:50 -0500

    preview colors

---
+++ scripts/rofi_run_2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rofi -show run \
+	-font "Hack Bold 9" \
+	-sidebar-mode \
+	-bw "2" \
+	-color-enabled \
+	-color-window "#182725,#6E6F69,#8A8881" \
+	-color-normal "#182725,#8A8881,#293939,#aa632f,#182725"
