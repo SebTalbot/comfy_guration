fb7c30d14899b149b194c047d276a2bbbc91b85c
Sebastien Talbot <moi@sebastientalbot.com>
2019-03-25 13:42:06 -0400

    [Cookit]

--- scripts/tex_yoda_trackpoint_config.sh
+++ scripts/tex_yoda_trackpoint_config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xinput set-prop 11 "Device Accel Profile" 7
-xinput set-prop 11 "Device Accel Velocity Scaling" 2.0
+xinput set-prop 12 "Device Accel Profile" 7
+xinput set-prop 12 "Device Accel Velocity Scaling" 2.0
