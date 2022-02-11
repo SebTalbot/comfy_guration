482fdbf49f93f38e9f432529136dfa57a5f45bb6
Sebastien Talbot <moi@sebastientalbot.com>
2022-02-11 04:44:31 -0500

    Nvidia fix tearing and refresh rate with dual monitors

--- scripts/home/lauch_screen.sh
+++ scripts/home/lauch_screen.sh
@@ -1,4 +1,9 @@
-xrandr --output DP-4 --mode 3440x1440 --primary --right-of DP-2
+# xrandr --output DP-4 --mode 3440x1440 --rate 120 --primary --right-of DP-2
+# Instead, use sudo nvidia-settings:
+# Set monitor rates, + Force Full Composition Pipeline (advance menu)
+# In OpenGL Settings: disable "Sync to VBlank" and "Allow Flipping"
+# Dont forget to set the environment (in ~/comfy_guration/dotfiles/home/environment) in /etc/environment
+nvidia-settings --load-config-only
 feh --bg-fill ~/pictures/wallpapers/pink_desert.jpg
 bash ~/comfy_guration/scripts/home/polybar.sh
 
