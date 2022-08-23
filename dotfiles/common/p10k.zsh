7fc723cce888cad8e7864e5d2c11234335a06778
Sebastien Talbot <moi@sebastientalbot.com>
2022-08-23 18:23:38 -0400

    WIP install software Manjaro i3 script

--- dotfiles/common/p10k.zsh
+++ dotfiles/common/p10k.zsh
@@ -83,7 +83,7 @@
     nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
     ranger                  # ranger shell (https://github.com/ranger/ranger)
     nnn                     # nnn shell (https://github.com/jarun/nnn)
-    vim_shell               # vim shell indicator (:sh)
+    # vim_shell               # vim shell indicator (:sh)
     midnight_commander      # midnight commander shell (https://midnight-commander.org/)
     nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     # vpn_ip                # virtual private network indicator
