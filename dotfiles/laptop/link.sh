0451acce18e144ff0bac5fa1e06fed46b02f0a8c
Sebastien Talbot <moi@sebastientalbot.com>
2022-09-29 17:20:29 -0400

    Laptop dotfiles extend link

--- dotfiles/laptop/link.sh
+++ dotfiles/laptop/link.sh
@@ -10,3 +10,9 @@ ln -sf $HOME/comfy_guration/dotfiles/laptop/i3_laptop $HOME/.config/i3/config
 
 safe_mkdir "$HOME/.config/polybar"
 ln -sf $HOME/comfy_guration/dotfiles/laptop/polybar_laptop $HOME/.config/polybar/config
+
+safe_mkdir "$HOME/.config/rofi"
+ln -sf $HOME/comfy_guration/dotfiles/laptop/rofi_config.rasi $HOME/.config/rofi/config.rasi
+
+safe_mkdir "$HOME/.config/kitty"
+ln -sf $HOME/comfy_guration/dotfiles/laptop/kitty.conf $HOME/.config/kitty/kitty.conf 
