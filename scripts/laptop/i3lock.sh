0125ff613d951b715fed711ec2f5081c663bfc65
Sebastien Talbot <moi@sebastientalbot.com>
2022-10-12 12:06:10 -0400

    laptop i3lock

--- scripts/laptop/i3lock.sh
+++ scripts/laptop/i3lock.sh
@@ -1,42 +1,56 @@
 #!/bin/bash
-A='#494a4574'  # dark
-B='#00000000'  # blank
-C='#494a4522'  # clear ish
-D='#ad6143ff'  # default
-T='#d8c29dff'  # text
-W='#b73c33ff'  # wrong
-V='#494a45ff'  # verifying
+# Need Blurlocker https://github.com/SafwanLjd/BlurLocker
+function prelock() {
+  killall -SIGUSR1 dunst
+  amixer -D pulse set Master 1+ mute
+  setxkbmap us
+}
 
-killall -s SIGSTOP notify-osd
-setxkbmap us
+function postlock() {
+  killall -SIGUSR2 dunst
+  amixer -D pulse set Master 1+ unmute
+}
 
-i3lock \
-  --insidever-color=$A   \
-  --ringver-color=$V     \
-  \
-  --insidewrong-color=$A \
-  --ringwrong-color=$W   \
-  \
-  --inside-color=$A \
-  --ring-color=$D        \
-  --line-color=$B        \
-  --separator-color=$D   \
-  \
-  --verif-color=$T        \
-  --wrong-color=$W        \
-  --time-color=$T        \
-  --date-color=$T        \
-  --layout-color=$T      \
-  --keyhl-color=$V       \
-  --bshl-color=$V        \
-  \
-  --screen 1            \
-  --blur 9              \
-  --clock               \
-  --indicator           \
-  --time-str="%H:%M:%S"  \
-  --date-str="%A, %m %Y" \
-  --keylayout 2         \
-  -n
+function lock() {
+  local A='#282828cc'  # dark
+  local B='#00000000'  # blank
+  local C='#28282822'  # clear ish
+  local D='#d79921'  # default
+  local T='#d79921'  # text
+  local W='#fb4934'  # wrong
+  local V='#458588'  # verifying
 
-killall -s SIGCONT notify-osd
+  blurlock \
+    --insidever-color=$A   \
+    --ringver-color=$V     \
+    \
+    --insidewrong-color=$A \
+    --ringwrong-color=$W   \
+    \
+    --inside-color=$A \
+    --ring-color=$D        \
+    --line-color=$A        \
+    --separator-color=$D   \
+    \
+    --verif-color=$T        \
+    --wrong-color=$W        \
+    --time-color=$T        \
+    --date-color=$T        \
+    --layout-color=$T      \
+    --keyhl-color=$V       \
+    --bshl-color=$V        \
+    \
+    --screen 1            \
+    --blur 9              \
+    --clock               \
+    --indicator           \
+    --time-str="%H:%M:%S"  \
+    --date-str="%A, %m %Y" \
+    --keylayout 2         \
+    -n
+}
+
+prelock
+lock
+postlock
+delpid
