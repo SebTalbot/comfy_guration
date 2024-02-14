d8ef1ec5641c55ba9ecffbc5fc1d37bf5df1267c
Sebastien Talbot <moi@sebastientalbot.com>
2024-02-13 19:04:03 -0500

    Dotfiles common cleanup

---
+++ dotfiles/common/_links.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source ../../scripts/manager/utils.sh
+
+u_link_file "PROMPT" "P10K" ~/comfy_guration/dotfiles/common/p10k ~/.p10k.zsh
+u_link_file "SHELL" "ZSH" ~/comfy_guration/dotfiles/common/zsh ~/.zshrc
