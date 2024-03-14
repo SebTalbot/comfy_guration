8cb4c848d349acd880dac2c0b801f3430cfaddf6
Sebastien Talbot <moi@sebastientalbot.com>
2024-03-14 01:18:21 -0400

    scripts laptop miffor_screen fix

--- scripts/laptop/mirror_screen.sh
+++ scripts/laptop/mirror_screen.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr --output eDP-1 --mode 1920x1080 --scale 1x1 --output HDMI-2 --mode 1920x1080 --scale 1x1 --same-as eDP-1
+xrandr --output eDP1 --mode 1920x1080 --scale 1x1 --output HDMI2 --mode 1920x1080 --scale 1x1 --same-as eDP1
