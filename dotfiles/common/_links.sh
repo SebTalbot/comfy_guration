aad1ca9e0000b6c317120b54d7b716297c8c66de
Sebastien Talbot <moi@sebastientalbot.com>
2024-12-19 02:27:03 -0500

    add: yazi

--- dotfiles/common/_links.sh
+++ dotfiles/common/_links.sh
@@ -5,3 +5,4 @@ u_link_file "PROMPT" "P10K" ~/comfy_guration/dotfiles/common/p10k ~/.p10k.zsh
 u_link_file "SHELL" "ZSH" ~/comfy_guration/dotfiles/common/zsh ~/.zshrc
 u_link_file "DIRECTORIES" "XDG" ~/comfy_guration/dotfiles/common/xdg_directories ~/.config/user-dirs.dirs
 u_link_file "MIRRORS" "REFLECTOR" ~/comfy_guration/dotfiles/common/reflector /etc/xdg/reflector/reflector.conf "sudo"
+u_link_file "TUI_FILES_MANAGER" "YAZI" ~/comfy_guration/dotfiles/common/yazi ~/.config/yazi
